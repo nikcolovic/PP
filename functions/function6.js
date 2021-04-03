36194ee564b2a84dd6e052b0c72afe63c62e24db
nikcolovic <nikcolovic@gmail.com>
2021-04-03 23:59:19 +0200

    update practis functions

---
+++ functions/function6.js
@@ -0,0 +1,188 @@
+/* 1. Write a function to count vowels in a provided string. 
+Vowels are a, e, i, o, and u as well as A, E, I, O, and U. */
+
+function vowelCount(str) {
+    var countV = 0;
+    for (var i = 0; i < str.length; i++) {
+        switch (str[i]) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                countV++
+                break;
+                    default:
+                break;
+        }
+    }
+    return countV
+}
+console.log(vowelCount('Write Arrays.'))
+
+   
+
+
+/* 2. Write a function that combines two arrays by alternatingly taking elements.
+
+[‘a’,’b’,’c’], [1,2,3] -> [‘a’,1,’b’,2,’c’,3] */
+
+function alternate(arr1, arr2) {
+    var altArr = [];
+    for (var i = 0; i < arr1.length; i++) {
+        altArr[altArr.length] = arr1[i];
+        altArr[altArr.length] = arr2[i];
+    }
+    return altArr
+}
+console.log(alternate(['a','b','c'], [1,2,3]))
+ 
+
+/* 3. Write a function that rotates a list by k elements.
+
+For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2]*/
+
+function rotate(arr,k) {
+    var rotArr = [];
+    var j = 0;
+    for (var i = 0; i < arr.length; i++){
+        if (i > k + 1) {
+            rotArr[i] = arr[j];
+            j++;  
+        }
+        else {
+            rotArr[i] = arr[i+k];
+        }
+    }
+    return rotArr;   
+}
+console.log(rotate([1,2,3,4,5,6],2));
+
+/*
+4. Write a function that takes a number and returns array of its digits. */
+
+function digit(num) {
+    var string = num + ''
+    var array = []
+    for (i = 0; i < string.length; i++){
+        array[i] = string[i]
+    }
+    return array
+}
+console.log(digit(123456))
+
+ 
+
+/*
+5. Write a program that prints a multiplication table for numbers up to 12. */
+
+function multiplicationTable(num){
+    var result = '';
+    for (i = 1; i <= num; i++){
+        
+        for (j = 1; j <= num; j++) {
+            result += '' +  i * j + ' ';   
+        }
+        result = result + '\n'
+    }
+    
+    return result
+}
+console.log(multiplicationTable(12))
+
+
+/*
+6. Write a function to input temperature in Centigrade and convert to Fahrenheit. 
+*/
+
+function centigradeToFahrenheit(C){
+    var F = (C * 9/5) + 32;
+    return F;
+ }
+console.log(centigradeToFahrenheit(10))
+/*
+7. Write a function to find the maximum element in array of numbers. Filter out all non-number elements. */
+function maxNum(array){
+    newArray = [];
+    for (i = 0; i < array.length; i++){
+        if (typeof array[i] === 'number'){
+            newArray[i] = array[i]
+        }
+    }
+   // var max = m[0];                                                                                         
+   // var m = 0;                                                
+    for (var i = 0; i < newArray.length; i++) {                            
+        if (m[i] >= max) {                                          
+            max = m[i];                                                                       
+        }
+    }
+    return max;
+}
+console.log(maxNum([1,2,3,2,1]))
+
+/*
+8. Write a function to find the maximum and minimum elements. Function returns an array. */
+
+ 
+
+/*
+9. Write a function to find the median element of array.*/
+
+ 
+
+/*
+10. Write a function to find the element that occurs most frequently. */
+
+ 
+
+/*
+11. Write a function to find and return the first, middle and last element of an 
+array if the array has odd number of elements. If number of elements is even, 
+return just the first and the last. In other cases (empty array), input array should be returned. */
+
+ 
+
+/*
+12. Write a function to find the average of N elements. 
+Make the function flexible to receive dynamic number or parameters. */
+
+ 
+
+/*
+Write a function to find all the numbers greater than the average. */
+
+ 
+
+/*
+The body mass index (BMI) is the ratio of the weight of a person (in kilograms) to the square of the height (in meters). Write a function that takes two parameters, weight and height, computes the BMI, and prints the corresponding BMI category:
+Starvation: less than 15
+Anorexic: less than 17.5
+Underweight: less than 18.5
+Ideal: greater than or equal to 18.5 but less than 25
+Overweight: greater than or equal to 25 but less than 30
+Obese: greater than or equal to 30 but less than 40
+Morbidly obese: greater than or equal to 40 */
+
+ 
+
+
+/*
+Write a function that takes a list of strings and prints them, one per line, in a rectangular frame.:
+
+For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
+*********
+* Hello *
+* World *
+* in    *
+* a     *
+* frame *
+*********
+*/
+ 
+
+
--- functions/functions2.js
+++ functions/functions2.js
@@ -44,9 +44,23 @@ console.log(concHaHaHa(5))
 
 /*
 4. Write a function to count the number of letter occurrences in a string.
-&quot;My random string&quot;, &quot;n&quot; -&gt; 2 */
+My random string;, "n"; --> 2 */
 
-  
+function letOccur(str, a){
+    var countletter = 0;
+    
+    for (var i=0; i < str.length; i++) {
+        if (str[i] === a) {
+            countletter ++
+        }
+    }
+    return countletter;
+}
+var string = 'My random string'
+var letter = 'n'
+
+console.log(letOccur(string, letter))
+
 
  
 /*
@@ -54,23 +68,78 @@ console.log(concHaHaHa(5))
 result should be the position of character. If there are no occurrences of the character, the
 function should return -1. */
 
+function firstOccur (string, char) {
+    var position = 0;
+    for (var i = 0; i < string.length; i++){
+    if (string[i] === char){
+        position = i + 1;
+        break
+    }
+    else position = -1
+    }
+    return position;
+}
+
+var string = 'My funny valentine'
+var char = 'n'
+
+console.log(firstOccur(string, char))
+
 /*
 6. Write a function to find the position of the last occurrence of a character in a string. The
 result should be in human numeration form. If there are no occurrences of the character,
 function should return -1. */ 
+function firstOccur (string, char) {
+    var position = 0;
+    for (var i = string.length - 1; i >= 0; i--){
+    if (string[i] === char){
+        position = i + 1;
+        break
+    }
+    else position = -1
+    }
+    return position;
+}
 
+var string = 'My funny valentine'
+var char = 'n'
+
+console.log(firstOccur(string, char))
 /*
 7. Write a function to convert string into an array. Space in a string should be represented as
 “null” in new array.
 &quot;My random string&quot; -&gt; [&quot;M&quot;, &quot;y&quot;, null, &quot;r&quot;, &quot;a&quot;]
 &quot;Random&quot; -&gt; [&quot;R&quot;, &quot;a&quot;, &quot;n&quot;, &quot;d&quot;, &quot;o&quot;, &quot;m&quot;] */
  
-
+function convert (stringToArray) {
+    arrayNew =[]; 
+    
+    for ( var i = 0; i < stringToArray.length; i++) {
+        if (stringToArray[i] === ' ') {
+            arrayNew[i] = null;
+        }
+        else {
+            arrayNew[i] = stringToArray[i]
+        }
+    }
+    return arrayNew
+}
+console.log(convert('My funny valentine'))
 /* 8. Write a function that accepts a number as a parameter and checks if the number is prime or
 not.
 Note: A prime number (or a prime) is a natural number greater than 1 that has no positive
 divisors other than 1 and itself. */
-
+function primeNum(p) {
+var isPrime = true;
+for (var i = 2; i < 10; i++){
+if (p % i === 0 && p !== i) {
+    isPrime = false;
+    break;
+    }
+    }
+    return isPrime;
+}
+console.log ('Is this prime number?' + primeNum(11))
 
  /*
 9. Write a function that replaces spaces in a string with provided separator. If separator is not
@@ -79,21 +148,133 @@ provided, use “-” (dash) as the default separator.
 &quot;My random string&quot;, &quot;+&quot; -&gt; &quot;My+random+string&quot;
 &quot;My random string&quot; -&gt; &quot;My-random-string&quot; */
 
+function replaceSpace(stringToSeparate,separator) {
+    if (separator === undefined) {
+        separator = '-';
+    }
+    stringNew = ''
+    for (var i = 0; i <stringToSeparate.length; i++) {
+        if (stringToSeparate[i] === ' ') {
+            stringNew += separator;
+        }
+        else {
+            stringNew += stringToSeparate[i];
+        }
+    }
+    return stringNew;
+}
+
+console.log(replaceSpace('My random string'));
+
+
 /*
 10. Write a function to get the first n characters and add “...” at the end of newly created string. */ 
 
+
+function beginOfString(stringToShort,n) {
+    var shortNewString = '';
+    for (var i = 0; i < n; i++) {
+        shortNewString += stringToShort[i];
+    }
+    shortNewString += '...';
+    return shortNewString;
+}
+
+console.log(beginOfString('A long, long time ago there was a small kingdom.', 17));
 /*
 11. Write a function that converts an array of strings into an array of numbers. Filter out all
 non-numeric values.
 [&quot;1&quot;, &quot;21&quot;, undefined, &quot;42&quot;, &quot;1e+3&quot;, Infinity] -&gt; [1, 21, 42, 1000] */
+
+function convertStrToNum(arrStr) {
+    var strNum = [];
+    var j = 0;
+    for (var i = 0; i < arrStr.length; i++) {
+        if (isFinite(parseFloat(arrStr[i]))) {
+        strNum[j] = parseFloat(arrStr[i]);
+        j++
+        }
+      //  console.log('Na poziciji i = ' + i +' Element stringa je' + arrStr[i] +  '\n');
+      //  console.log('Trenutna pozicija j = ' + (j - 1) +' Novi niz je ' + strNum +  '\n');
+    }
+    return strNum;
+}
+
+console.log('New string without non-numeric values is: ');
+console.log(convertStrToNum(["1", "21", undefined, "42", "1e+3", Infinity, '6']));
+
 
 /*
 12. Write a function to calculate how many years there are left until retirement based on the
 year of birth. Retirement for men is at age of 65 and for women at age of 60. If someone is
 already retired, a proper message should be displayed. */
 
+function pension(birth,currentY,gender) {
+    var ret;
+    if (gender === 'man') {
+        ret = 65 - (currentY - birth);
+    }
+    else if (gender === 'woman') {
+        ret = 60 - (currentY - birth);
+    }
+    else {
+        return 'Please check input.';
+    }
+    if (ret > 0) {
+        return ret + ' years to retirement';
+    }
+    else {
+        return 'Already retired'
+    }
+
+}
+
+console.log(pension(1981,2021,'man'));
+console.log(pension(1960,2021,'woman'));
 /*
 13. Write a function to humanize a number (formats a number to a human-readable string) with
 the correct suffix such as 1st, 2nd, 3rd or 4th.
 1 -&gt; 1st
 11 -&gt; 11th */
+function humanize(num) {
+    var hum;
+    if (num > 10 && num < 14) {
+        hum = num + 'th'
+    }
+    else {
+        moduo = num%10;
+        switch (moduo) {
+            case 1:
+                hum = num + 'st';
+                break;
+            case 2:
+                hum = num + 'nd';
+                break;
+            case 3:
+                hum = num + 'rd';
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:    
+            case 0:
+                hum = num + 'th';
+                break;        
+        }
+    }
+    return hum;
+}
+
+console.log(humanize(11));
+console.log(humanize(12));
+console.log(humanize(13));
+console.log(humanize(1));
+console.log(humanize(2));
+console.log(humanize(3));
+console.log(humanize(10));
+console.log(humanize(1021));
+console.log(humanize(100));
+
+
