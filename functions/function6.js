643252091a9a822068293f3d71dbba9cdb0e757f
nikcolovic <nikcolovic@gmail.com>
2021-04-16 21:51:58 +0200

    new task done

--- functions/function6.js
+++ functions/function6.js
@@ -86,7 +86,7 @@ function multiplicationTable(num){
     for (i = 1; i <= num; i++){
         
         for (j = 1; j <= num; j++) {
-            result += '' +  i * j + ' ';   
+            result += ' ' +  i * j + ' ';   
         }
         result = result + '\n'
     }
@@ -256,3 +256,42 @@ For example the list ["Hello", "World", "in", "a", "frame"] gets printed as:
 * frame *
 *********
 */
+
+function makeFrame(array) {
+	var maxWordLength = 0;
+	var topAndBottomString = '';
+	var topAndBottomlength = 0;
+	var middlePart = '';
+	var spaceLength = 0;
+	var spaceString = '';
+	var frame = '';
+	var word = '';
+	function makeStringFromChar(char, num) {
+		var resultString = '';
+		for (var i = 0; i < num; i++) {
+			resultString += char;
+		}
+		return resultString;
+	}
+	//find the longest word
+	for (var i = 0; i < array.length; i++) {
+		word = array[i];
+		if (word.length > maxWordLength) {
+			maxWordLength = word.length;
+		}
+	}
+	//make top and bottom strings
+	topAndBottomlength = maxWordLength + 4;
+	topAndBottomString = makeStringFromChar('*', topAndBottomlength);
+	//create middle part
+	for (var k = 0; k < array.length; k++) {
+		word = array[k];
+		spaceLength = maxWordLength - word.length;
+		spaceString = makeStringFromChar(' ', spaceLength);
+		middlePart += '* ' + word + spaceString + ' *\n';
+	}
+	//create frame
+	frame = topAndBottomString + '\n' + middlePart + topAndBottomString;
+	return frame;
+}
+console.log(makeFrame(['Hello', 'world', 'in', 'a', 'frame']))
